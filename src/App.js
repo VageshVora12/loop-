2ff27dc434846cdabc417b1f9602504a570a7424
Swapnil <swapnil@ksoftpl.com>
2022-04-12 14:57:38 +0530

    pause play controls disabled

--- src/App.js
+++ src/App.js
@@ -4,21 +4,15 @@ import React,{useCallback,useEffect,useRef,useState} from 'react';
 import VideoLooper from 'react-video-looper';
 import sampleVideo from './vid.mp4';
 function App() {
-  const ref = useRef(null);
-
   const handle = useFullScreenHandle();
-useEffect(() => {
-  setTimeout(() => {
-    ref.current.click();
-  }, 5000); //miliseconds
-}, []);
+
   return (
     <div>
 
       <FullScreen handle={handle}>
-      <VideoLooper source={sampleVideo} start={0} end={57} style={{position:'absolute'}} />
+      <VideoLooper source={sampleVideo} start={0} end={57} style={{position:'absolute',pointerEvents: "none"}} />
       </FullScreen>
-      <button ref={ref} onClick={handle.enter} style={{position:'absolute',opacity:'0.01',width:'100%',height:'100vh'}}>
+      <button onClick={handle.enter} style={{position:'absolute',opacity:'0.01',width:'100%',height:'100vh'}}>
         Enter fullscreen
       </button>
       
